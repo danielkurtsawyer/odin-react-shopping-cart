f66f6c6d839563404adac48861b6203176b011fd
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-07-11 15:37:14 -0400

    Refactor browser routing to use children paths and errorElement

--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -4,15 +4,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Shopping from "./pages/Shopping.jsx";
+import Cart from "./pages/Cart.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "shopping",
-    element: <Shopping />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "shop", element: <Shopping /> },
+      { path: "cart", element: <Cart /> },
+    ],
   },
 ]);
 
