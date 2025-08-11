eb0c6905464c4d864dbfc6b6ac7c68ab21a2e6a3
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-08-11 15:56:15 -0400

    Set up vitest, React Testing Library, and user event testing library, and create Test Suite for ItemCard

---
+++ shopping-cart/src/App.test.jsx
@@ -0,0 +1,12 @@
+import { describe, it, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+describe("App", () => {
+  beforeEach(() => {});
+  it("renders headline", () => {
+    // check if App components renders headline
+  });
+});
---
+++ shopping-cart/src/components/ItemCard/ItemCard.test.jsx
@@ -0,0 +1,131 @@
+import { vi, describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ItemCard from "./ItemCard";
+import { input } from "@testing-library/user-event/dist/cjs/event/input.js";
+
+describe("ItemCard", () => {
+  it("should render an item card with the correct product information and starting quantity of one", () => {
+    render(
+      <ItemCard
+        id={1}
+        title="Test Title"
+        price={49.99}
+        image="https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+      />
+    );
+
+    const image = screen.getByAltText("Test Title");
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      "src",
+      "https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+    );
+
+    const title = screen.getByText("Test Title");
+    expect(title).toBeInTheDocument();
+
+    const price = screen.getByText("$49.99");
+    expect(price).toBeInTheDocument();
+
+    const input = screen.getByRole("spinbutton", { value: { now: 1 } });
+    expect(input).toBeInTheDocument();
+
+    const button = screen.getByRole("button", { name: "Add to Cart" });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should call the onClick function when clicked", async () => {
+    const onClick = vi.fn();
+    const user = userEvent.setup();
+    render(
+      <ItemCard
+        id={1}
+        title="Test Title"
+        price={49.99}
+        image="https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+        addToCart={onClick}
+      />
+    );
+
+    const button = screen.getByRole("button", { name: "Add to Cart" });
+    await user.click(button);
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("should not call the onClick function when it isn't clicked", async () => {
+    const onClick = vi.fn();
+    render(
+      <ItemCard
+        id={1}
+        title="Test Title"
+        price={49.99}
+        image="https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+        addToCart={onClick}
+      />
+    );
+
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it("should increment quantity when the + img is clicked", async () => {
+    const user = userEvent.setup();
+    render(
+      <ItemCard
+        id={1}
+        title="Test Title"
+        price={49.99}
+        image="https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+      />
+    );
+
+    const incrementImg = screen.getByAltText("+");
+
+    const input = screen.getByRole("spinbutton", { value: { now: 1 } });
+    expect(input).toBeInTheDocument();
+
+    await user.click(incrementImg);
+    expect(input.ariaValueNow).toEqual("2");
+  });
+
+  it("should decrement quantity when the - img is clicked", async () => {
+    const user = userEvent.setup();
+    render(
+      <ItemCard
+        id={1}
+        title="Test Title"
+        price={49.99}
+        image="https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+        initialQuantity={2}
+      />
+    );
+
+    const decrementImg = screen.getByAltText("-");
+
+    const input = screen.getByRole("spinbutton", { value: { now: 2 } });
+    expect(input).toBeInTheDocument();
+
+    await user.click(decrementImg);
+    expect(input.ariaValueNow).toEqual("1");
+  });
+
+  it("should not decrement quantity when the - img is clicked if quantity is 1", async () => {
+    const user = userEvent.setup();
+    render(
+      <ItemCard
+        id={1}
+        title="Test Title"
+        price={49.99}
+        image="https://www.wildblueyondermusic.com/assets/logo-B4oS1OYH.png"
+      />
+    );
+
+    const decrementImg = screen.getByAltText("-");
+
+    const input = screen.getByRole("spinbutton", { value: { now: 1 } });
+    expect(input).toBeInTheDocument();
+
+    await user.click(decrementImg);
+    expect(input.ariaValueNow).toEqual("1");
+  });
+});
