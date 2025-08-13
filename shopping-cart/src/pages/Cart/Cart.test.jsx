d650f64e4c3dc84694b6059ba8bb632732f3ad61
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-08-13 19:50:40 -0400

    Remove unneeded import

--- shopping-cart/src/pages/Cart/Cart.test.jsx
+++ shopping-cart/src/pages/Cart/Cart.test.jsx
@@ -1,6 +1,5 @@
 import { vi, describe, it, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { useOutletContext } from "react-router-dom";
 
 import Cart from "./Cart";
